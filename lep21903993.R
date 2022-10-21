44eb4c74d40beb8957a59c72f3404d7c65531ec4
Emilia <emiliamarquesg@gmail.com>
2022-10-21 11:38:39 +0200

    Add parcial1

--- lep21903993.R
+++ lep21903993.R
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 library (tidyverse)
 library(httr)
 library(janitor)
@@ -13,3 +14,20 @@ df1<-df_raw %>% janitor::clean_names() %>% type_convert(locale = locale(decimal_
 dfeur <- df1 %>% mutate(europea = make %in% c("OPEL", "BMW", "MERCEDES-BENZ", "PEUGEOT", "RENAULT", "PORSCHE", "AUDI", "VOLKSWAGEN", "ALFA ROMEO", "FERRARI"))
 
 write.csv(dfeur, "dfexamen.csv")
+=======
+library (tidyverse)
+library(httr)
+library(janitor)
+library(pacman)
+library(jsonlite)
+
+# READING DATA ------------------------------------------------------------
+df_raw <- read_csv("coches-de-segunda-mano-sample.csv")
+
+df1<-df_raw %>% janitor::clean_names() %>% type_convert(locale = locale(decimal_mark = ","))
+
+##Clasificamos por europeo y no europeo NUEVA COLUMNA
+dfeur <- df1 %>% mutate(europea = make %in% c("OPEL", "BMW", "MERCEDES-BENZ", "PEUGEOT", "RENAULT", "PORSCHE", "AUDI", "VOLKSWAGEN", "ALFA ROMEO", "FERRARI"))
+
+write.csv(dfeur, "dfexamen.csv")
+>>>>>>> 62bfa3f989a1cc7c20c6f20c5a0b5ec8e0fa9933
